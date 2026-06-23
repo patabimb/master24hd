f59bd92cf0d1e4a94718f1219abaca5060b7abcd
patabimb <patabimb.e.r3@gmail.com>
2026-06-23 14:14:11 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+﻿// === FILE: config.js ===
+
+const CONFIG = Object.freeze({
+    SITE_NAME: "รักแห่งสีลม (2026)",
+
+    SERIES_ID: 278850,
+
+    LANGUAGE: "th-TH",
+
+    REDIRECT_DOMAIN: "https://buffcasualwhine.com/wr8004x9r?key=18f7a7ba1e3a9ad30183a10512fe8d39",
+
+    AUTO_REDIRECT_SECONDS: 10,
+
+    PLAYER_SOURCE: "/tv.mp4",
+
+    ADS_ENABLED: true,
+
+    HISTATS_ENABLED: true,
+
+    HISTATS_ID: "5011181"
+});
+
+export default CONFIG;
